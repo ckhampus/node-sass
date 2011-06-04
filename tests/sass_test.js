4f8c0ce8db8977527d9190c6b6dc035654125404
Cristian Hampus <contact@cristianhampus.se>
2011-06-04 15:13:59 +0200

    Added package.json

--- tests/sass_test.js
+++ tests/sass_test.js
@@ -3,3 +3,7 @@ var sass = require('../lib/sass.js');
 sass.compile('./style.scss', function (css) {
     console.log(css);
 });
+
+sass.compile('./style.scss', {'outputStyle':'expanded'}, function (css) {
+    console.log(css);    
+});
